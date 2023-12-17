0c3cf56cc3e8f29a35fa8d29118189cded28eed1
paduchd <paduchdenis@gmail.com>
2023-12-17 13:14:36 +0100

    added outskirts decorations

--- Assets/Scripts/EventManagers/SceneSoundManager.cs
+++ Assets/Scripts/EventManagers/SceneSoundManager.cs
@@ -8,18 +8,20 @@ public class SceneSoundManager : MonoBehaviour
     [SerializeField] private AudioClip[] bushSounds;
     [SerializeField] private AudioClip boxFall;
     [SerializeField] private AudioClip eagleAttack;
-    [SerializeField] private AudioClip fallInWater;
     [SerializeField] private AudioClip foodCollect;
     [SerializeField] private AudioClip valveTurn;
+    [SerializeField] private AudioClip containerSearch;
     
     private void OnEnable()
     {
         audioComponent = GetComponent<AudioSource>();
+        audioComponent.volume = 0.1f;
         BushController.OnBushEnter += PlayBushSound;
         PlayerEventManager.OnBoxFall += PlayBoxSound;
         PlayerEventManager.OnFoodEaten += PlayFoodSound;
         Eagle.EagleSound += PlayEagleSound;
         ValveInteraction.OnValveInteraction += PlayValveSound;
+        SearchContainer.OnContainerSearch += PlaySearchSound;
     }
 
     private void OnDisable()
@@ -33,32 +35,31 @@ public class SceneSoundManager : MonoBehaviour
 
     private void PlayBushSound()
     {
-        audioComponent.volume = 0.1f;
         audioComponent.PlayOneShot(bushSounds[Random.Range(0, bushSounds.Length)]);
     }
 
     private void PlayBoxSound()
     {
-        audioComponent.volume = 0.01f;
         audioComponent.PlayOneShot(boxFall);
     }
 
     private void PlayEagleSound()
     {
-        audioComponent.volume = 0.1f;
         audioComponent.PlayOneShot(eagleAttack);
     }
 
     private void PlayFoodSound(bool b)
     {
-        audioComponent.volume = 0.1f;
         audioComponent.PlayOneShot(foodCollect);
     }
 
     private void PlayValveSound()
     {
-        audioComponent.volume = 0.1f;
         audioComponent.PlayOneShot(valveTurn);
     }
-    
+
+    private void PlaySearchSound()
+    {
+        audioComponent.PlayOneShot(containerSearch);
+    }
 }
